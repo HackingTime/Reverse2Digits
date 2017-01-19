1356b014b0ca9004e3cadaf239d116b7a9b51c7d
HackingTime <gcgithub@bar.edu>
2017-01-19 15:51:06 -0800

    modified main file

--- reverse2digits.c
+++ reverse2digits.c
@@ -7,12 +7,12 @@ int main()
 int i;
 int j;
 int k;
-
+printf("Enter digits to be reversed(two only!): ");
 scanf("%d", &i);
 
 j = i % 10; 
 k = i / 10; 
 
-printf("%d%d", j, k);
+printf("%d%d\n", j, k);
 
 }
